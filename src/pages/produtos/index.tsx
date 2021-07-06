1d2c0118d232db12fcf78a98910ca3ed1a5b4f57
DenisVasconcellos <denis_mvasconcellos@hotmail.com>
2021-07-06 19:31:03 -0300

    comit  iniciando

---
+++ src/pages/produtos/index.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+import { useState } from 'react';
+import api from '../../services/api';
+
+interface IProduct {
+    Title: string;
+    Year: string;
+    imdbID: string;
+    Type: string;
+    Poster: string;
+}
+
+const Produtos = () => {
+    const [products, setProducts] = useState<IProduct[]>([]);
+
+    const loadingProducts = async () => {
+        const results = await api.get(`/?apikey=39f74d66&s=bat`);
+        setProducts(results.data.Search);
+    };
+
+    useEffect(() => {
+        loadingProducts();
+    }, []);
+
+    return(
+        <>
+            <h1>Produtos</h1>
+            <ul>
+                {products.map(p => {
+                    return(
+                        <li>{p.Title}</li>
+                    );
+                })}
+            </ul>
+        </>
+    );
+}
+export default Produtos;
